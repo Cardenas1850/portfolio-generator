ad608013696415488cc18a5124cb186e0d3cc778
Jason Cardenas <Jason.Cardenas@ftcsc.org>
2022-04-20 12:22:41 -0400

    added project quesitons

--- app.js
+++ app.js
@@ -9,13 +9,73 @@ const inquirer = require('inquirer');
 //
 //    console.log('Portfolio Complete! Checkout index.html to see the output!');
 //});
+const promptUser = () => {
+    return inquirer.prompt([{
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        },
 
-inquirer
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your github username'
+        },
 
-    .prompt([{
-    type: 'input',
-    name: 'name',
-    message: 'What is your name?'
-}])
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some inforamtion about yourself'
+        }
 
-.then(answers => console.log(answers));
+    ]);
+};
+
+const promptProject = () => {
+    console.log(`
+    =================
+    Add a New Project
+    =================
+    `);
+
+    return inquirer.prompt([{
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'What is the description of your project?'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['Javascript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the Github link to your project (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+        }
+    ]);
+};
+
+promptUser()
+    .then(answers => console.log(answers))
+    .then(promptProject)
+    .then(projectAnswers => console.log(projectAnswers));
