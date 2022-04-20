758332c9826da808cfd4a0fed33895cc28331358
Jason Cardenas <Jason.Cardenas@ftcsc.org>
2022-04-20 10:19:37 -0400

    create page-templete.js file

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs')
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
 const [name, github] = profileDataArgs;
 
-const generatePage = (name, github) => {
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-    </head>
-
-    <body>
-    <h1>${name}</h1>
-    <h2><a href="href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-
-};
-console.log(name, github);
-console.log(generatePage(name, github));
-
-
-commit before leaving work
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+    console.log('Portfolio complete! Check out index.html to see the output');
+});
